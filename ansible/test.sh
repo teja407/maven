9107878f58531fec41353edda0d426aa4a832531
teja407 <tejaatla123@gmail.com>
2019-02-08 14:42:11 +0530

    Update test.sh

--- ansible/test.sh
+++ ansible/test.sh
@@ -1,4 +1,4 @@
 for i in `cat inv`
 do
-sudo ansible $i -m copy -a "src=/var/lib/jenkins/workspace/maven/copy.yml dest=/home/ec2-user"
+sudo ansible $i -m copy -a "src=/var/lib/jenkins/workspace/maven/target/maven-1.0-SNAPSHOT.jar dest=/home/ec2-user"
 done
